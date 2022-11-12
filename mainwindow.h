986a070d87e7b0a2660c216d53f88bbd293cbaa9
lolainta <lolainta@gmail.com>
2022-11-12 15:55:30 +0800

    style: format source

--- mainwindow.h
+++ mainwindow.h
@@ -14,41 +14,42 @@
 #include <QTabWidget>
 #include <QTableWidget>
 #include <QHeaderView>
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui{
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow:public QMainWindow{
     Q_OBJECT
-    QTreeWidget *local_site;
-    QComboBox *local_memo;
-    QStringList *local_memo_dir;
+    QTreeWidget*local_site;
+    QComboBox*local_memo;
+    QStringList*local_memo_dir;
 
-    QTreeWidget *remote_site;
-    QComboBox *remote_memo;
-    QStringList *remote_memo_dir;
+    QTreeWidget*remote_site;
+    QComboBox*remote_memo;
+    QStringList*remote_memo_dir;
 
-    QTreeWidget *local_file_view;
-    QTreeWidget *remote_file_view;
+    QTreeWidget*local_file_view;
+    QTreeWidget*remote_file_view;
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget*parent=nullptr);
     ~MainWindow();
     void mainmenu();
-    QHBoxLayout* input_field();
+    QHBoxLayout*input_field();
     void local_view();
     void remote_view();
-    QHBoxLayout* memo_box();
-    QWidget* status();
+    QHBoxLayout*memo_box();
+    QWidget*status();
 
 public slots:
-    void click_item_inDir(QTreeWidgetItem *);
+    void click_item_inDir(QTreeWidgetItem*);
     void dir_combo_changed(const QString&);
-    void remote_click_item_inDir(QTreeWidgetItem *);
+    void remote_click_item_inDir(QTreeWidgetItem*);
     void remote_dir_combo_changed(const QString&);
-
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow*ui;
 };
 #endif // MAINWINDOW_H
